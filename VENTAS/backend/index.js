c67408ff564741e3355f69c0a7ca78490d1e55e1
TochielVro <heidan.toribio@gmail.com>
2025-06-03 20:48:22 -0500

    Modificacion de la logica de conexion

--- VENTAS/backend/index.js
+++ VENTAS/backend/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const productosRoutes = require('./routes/productos.routes');
+const productosRoutes = require('./Routes/productos.routes');
+const usuariosRoutes = require('./Routes/usuarios.routes');
+const clientesRoutes = require('./Routes/clientes.routes');
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/productos', productosRoutes);
-app.listen(3306, () => {
-console.log('Servidor corriendo en http://localhost:3001');
+app.use('/api/usuarios', usuariosRoutes);
+app.use('/api/clientes', clientesRoutes);
+
+app.listen(3001, () => {
+  console.log('Servidor corriendo en http://localhost:3001');
 });
+
+// Logica
